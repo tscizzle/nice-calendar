740cfac1a3465be35fe94926b014d833dae41791
Tyler <tscizzle@gmail.com>
2018-11-14 10:47:49 -0500

    moved occurrence to own file, fixed scss deprecation warning

--- src/components/calendar-cell/calendar-cell.js
+++ src/components/calendar-cell/calendar-cell.js
@@ -5,7 +5,6 @@ import moment from 'moment-timezone';
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { upsertOccurrence, deleteOccurrence } from 'api';
 import withUser from 'state-management/state-connectors/with-user';
 import withEvents from 'state-management/state-connectors/with-events';
 import withOccurrences from 'state-management/state-connectors/with-occurrences';
@@ -18,9 +17,10 @@ import {
   eventShape,
   makeNewEventDoc,
   getScheduledOccurrences,
-  getNextScheduledOccurrence,
 } from 'models/event';
 import { occurrenceShape } from 'models/occurrence';
+
+import CalendarOccurrence from 'components/calendar-occurrence/calendar-occurrence';
 import { CircleButton } from 'components/nice-button/nice-button';
 
 import 'stylesheets/components/calendar-cell/calendar-cell.css';
@@ -183,150 +183,6 @@ CalendarCell = _.flow([
 
 export default CalendarCell;
 
-class CalendarOccurrence extends Component {
-  static propTypes = {
-    event: eventShape.isRequired,
-    occurrence: occurrenceShape.isRequired,
-    hasOccurred: PropTypes.bool,
-    loggedInUser: userShape.isRequired,
-    timezone: PropTypes.string.isRequired,
-    fetchOccurrences: PropTypes.func.isRequired,
-    editingEventFormData: eventShape,
-    setEditingEventFormData: PropTypes.func.isRequired,
-    nowMinute: PropTypes.instanceOf(Date).isRequired,
-  };
-
-  state = {
-    isHovered: false,
-  };
-
-  setIsHovered = () => this.setState({ isHovered: true });
-
-  setIsNotHovered = () => this.setState({ isHovered: false });
-
-  openEditingEventForm = () => {
-    const { event, timezone, setEditingEventFormData, nowMinute } = this.props;
-    const nextScheduledOccurrence = getNextScheduledOccurrence({
-      event,
-      timezone,
-      now: nowMinute,
-    });
-    const newDatetime = nextScheduledOccurrence
-      ? nextScheduledOccurrence.occurrence.datetime
-      : event.datetime;
-    const newEvent = {
-      ...event,
-      datetime: newDatetime,
-    };
-    setEditingEventFormData({ event: newEvent });
-  };
-
-  deleteOccurrence = evt => {
-    evt.stopPropagation();
-    const { occurrence, loggedInUser, fetchOccurrences } = this.props;
-    const occurrenceId = occurrence._id;
-    deleteOccurrence({ occurrenceId }).then(() => {
-      fetchOccurrences({ user: loggedInUser });
-    });
-  };
-
-  toggleCheckOffOccurrence = evt => {
-    evt.stopPropagation();
-    const { occurrence, loggedInUser, fetchOccurrences } = this.props;
-    const newOccurrence = {
-      ...occurrence,
-      checkedOff: !occurrence.checkedOff,
-    };
-    upsertOccurrence({ occurrence: newOccurrence }).then(() => {
-      fetchOccurrences({ user: loggedInUser });
-    });
-  };
-
-  render() {
-    const {
-      event,
-      occurrence,
-      hasOccurred,
-      timezone,
-      editingEventFormData,
-    } = this.props;
-    const { isHovered } = this.state;
-    const isBeingEdited =
-      editingEventFormData && editingEventFormData._id === event._id;
-    const text =
-      event.title || (isBeingEdited ? '(Adding event…)' : '(Untitled event)');
-    const toggleCheckOffIcon = occurrence.checkedOff ? 'times' : 'check';
-    const timeString = moment(occurrence.datetime)
-      .tz(timezone)
-      .format('HH:mm');
-    const calendarOccurrenceClasses = classNames('calendar-occurrence', {
-      'being-edited': isBeingEdited,
-      'has-occurred': hasOccurred,
-      'checked-off': occurrence.checkedOff,
-    });
-    const calendarOccurrenceDetailsClasses = classNames(
-      'calendar-occurrence-details',
-      {
-        'is-hovered': isHovered,
-      }
-    );
-    const calendarOccurrencePreviewClasses = classNames(
-      'calendar-occurrence-preview',
-      {
-        'is-hovered': isHovered,
-      }
-    );
-    return (
-      <div
-        className={calendarOccurrenceClasses}
-        onClick={this.openEditingEventForm}
-        onMouseEnter={this.setIsHovered}
-        onMouseLeave={this.setIsNotHovered}
-      >
-        <div className={calendarOccurrenceDetailsClasses}>
-          {hasOccurred ? (
-            <div className="calendar-occurrence-actions">
-              <FontAwesomeIcon
-                icon="trash"
-                className="calendar-occurrence-action-button"
-                size="sm"
-                onClick={this.deleteOccurrence}
-              />
-              <FontAwesomeIcon
-                icon={toggleCheckOffIcon}
-                className="calendar-occurrence-action-button"
-                size="sm"
-                onClick={this.toggleCheckOffOccurrence}
-              />
-            </div>
-          ) : (
-            <div />
-          )}
-          {timeString}
-        </div>
-        <div className={calendarOccurrencePreviewClasses}>
-          {!hasOccurred &&
-            event.isRecurring && (
-              <FontAwesomeIcon
-                icon="clock"
-                size="sm"
-                className="recurring-occurrence-icon"
-              />
-            )}
-          {text}
-        </div>
-      </div>
-    );
-  }
-}
-
-CalendarOccurrence = _.flow([
-  withUser,
-  withOccurrences,
-  withEditingEventFormData,
-  withNowMinute,
-])(CalendarOccurrence);
-
 let CalendarCellEditEventButton = ({
   startDatetime,
   endDatetime,
@@ -339,24 +195,21 @@ let CalendarCellEditEventButton = ({
 }) => {
   const openEditingEventForm = () => {
     const startMoment = moment(startDatetime).tz(timezone);
+    const endMoment = moment(endDatetime).tz(timezone);
     const nowMinuteMoment = moment(nowMinute).tz(timezone);
-    const endOfTodayMoment = nowMinuteMoment.clone().endOf('day');
-    const isFutureCell = startMoment.isAfter(endOfTodayMoment);
+    const isFutureCell = startMoment.isAfter(nowMinuteMoment);
     let datetime;
     if (isFutureCell) {
-      datetime = startMoment
-        .clone()
-        .set({ hours: 12 })
-        .toDate();
+      datetime = startDatetime;
     } else {
-      const candidateStartMoment = nowMinuteMoment
+      const candidateEventMoment = nowMinuteMoment
         .clone()
         .add(5, 'minutes')
         .startOf('minute');
-      const startMoment = candidateStartMoment.isAfter(endOfTodayMoment)
-        ? endOfTodayMoment
-        : candidateStartMoment;
-      datetime = startMoment.toDate();
+      const eventMoment = candidateEventMoment.isAfter(endMoment)
+        ? nowMinuteMoment
+        : candidateEventMoment;
+      datetime = eventMoment.toDate();
     }
     const suppliedEvent = {
       ...(isEditingExistingEvent ? {} : editingEventFormData),
@@ -373,7 +226,8 @@ let CalendarCellEditEventButton = ({
 };
 
 CalendarCellEditEventButton.propTypes = {
-  containedDatetime: PropTypes.instanceOf(Date).isRequired,
+  startDatetime: PropTypes.instanceOf(Date).isRequired,
+  endDatetime: PropTypes.instanceOf(Date).isRequired,
   loggedInUser: userShape.isRequired,
   timezone: PropTypes.string.isRequired,
   editingEventFormData: eventShape,
