07782309b016a2ae324efacec617db49f4bdfe33
Tyler <tscizzle@gmail.com>
2018-11-18 20:58:52 -0500

    got rid of deprecation warnings

--- server/config/mongo-database.js
+++ server/config/mongo-database.js
@@ -3,15 +3,25 @@ const mongoose = require('mongoose');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const MONGO_CONNECTION_STRING =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017';
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/nice-calendar';
 
 mongoose.Promise = global.Promise;
 
 if (isProduction) {
   mongoose.connect(
     MONGO_CONNECTION_STRING,
-    { config: { autoIndex: false } }
+    {
+      config: { autoIndex: false },
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }
   );
 } else {
-  mongoose.connect(MONGO_CONNECTION_STRING);
+  mongoose.connect(
+    MONGO_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }
+  );
 }
