46e0b5cc1781bb6171e6695b3c052a629119da69
Tyler S-C <tscizzle@gmail.com>
2019-10-06 18:03:43 -0400

    catch connection error, and retry indefinitely for reconnecting

--- server/config/mongo-database.js
+++ server/config/mongo-database.js
@@ -1,31 +1,27 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
 const connectMongo = () => {
   const isProduction = process.env.NODE_ENV === 'production';
 
   const MONGO_CONNECTION_STRING =
     process.env.MONGODB_URI || 'mongodb://localhost:27017/nice-calendar';
 
-  mongoose.Promise = global.Promise;
-
+  // configure mongo options
+  const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    reconnectTries: Number.MAX_VALUE,
+  };
   if (isProduction) {
-    mongoose.connect(
-      MONGO_CONNECTION_STRING,
-      {
-        config: { autoIndex: false },
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    );
-  } else {
-    mongoose.connect(
-      MONGO_CONNECTION_STRING,
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    );
+    options.config = { autoIndex: false };
   }
+
+  // connect to the mongo database
+  mongoose
+    .connect(MONGO_CONNECTION_STRING, options)
+    .catch(err => console.log(`Error connecting to mongo: ${err}`));
 };
 
 module.exports = connectMongo;
